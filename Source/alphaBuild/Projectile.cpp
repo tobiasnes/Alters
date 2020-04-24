38bf92b5554db797b1103bfd3bd40e51d2025004
BloodyFM <58073302+BloodyFM@users.noreply.github.com>
2020-04-24 19:05:20 +0200

    added Projectile c++ class
    Added and tested a c++ class that will be used for our projectiles

---
+++ Source/alphaBuild/Projectile.cpp
@@ -0,0 +1,94 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Projectile.h"
+#include "Components/SphereComponent.h"
+#include "Components/StaticMeshComponent.h"
+#include "Particles/ParticleSystemComponent.h"
+#include "Kismet/GameplayStatics.h"
+#include "Engine/World.h"
+#include "Sound/SoundCue.h"
+#include "Main.h"
+#include "Enemy.h"
+
+// Sets default values
+AProjectile::AProjectile()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	HitBox = CreateDefaultSubobject<USphereComponent>(TEXT("HitBox"));
+	SetRootComponent(HitBox);
+
+	Mesh = CreateAbstractDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh"));
+	Mesh->SetupAttachment(GetRootComponent());
+
+	IdleParticlesComponent = CreateAbstractDefaultSubobject<UParticleSystemComponent>(TEXT("IdleParticlesComponent"));
+	IdleParticlesComponent->SetupAttachment(GetRootComponent());
+
+	Damage = 10.f;
+	KnockBack = 500.f;
+	MovementSpeed = 200.f;
+	MaxLifeSpan = 5.f;
+	CurrentLifeSpan = 0.f;
+	bDestroyOnHit = false;
+
+}
+
+// Called when the game starts or when spawned
+void AProjectile::BeginPlay()
+{
+	Super::BeginPlay();
+
+	ForwardVector = GetActorForwardVector();
+	
+	HitBox->OnComponentBeginOverlap.AddDynamic(this, &AProjectile::OnOverlapBegin);
+	HitBox->OnComponentEndOverlap.AddDynamic(this, &AProjectile::OnOverlapEnd);
+
+}
+
+// Called every frame
+void AProjectile::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	CurrentLifeSpan += DeltaTime;
+	if (CurrentLifeSpan >= MaxLifeSpan)
+	{
+		Destroy();
+	}
+	SetActorLocation(GetActorLocation() + (ForwardVector * DeltaTime * MovementSpeed));
+}
+
+void AProjectile::OnOverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	UE_LOG(LogTemp, Warning, TEXT("OnOverlapBegin()"));
+
+	if (OtherActor)
+	{
+		AMain* Main = Cast<AMain>(OtherActor);
+		if (Main)
+		{
+			FVector Direction = GetActorLocation() - Main->GetActorLocation();
+			Main->TakeDMG(Damage, KnockBack, ForwardVector + FVector(0.f, 0.f, 0.1f));
+		}
+	}
+
+	if (OverlapParticles)
+	{
+		UGameplayStatics::SpawnEmitterAtLocation(GetWorld(), OverlapParticles, GetActorLocation(), FRotator(0.f), true);
+	}
+	if (OverlapSound)
+	{
+		UGameplayStatics::PlaySound2D(this, OverlapSound);
+	}
+	if (bDestroyOnHit)
+	{
+		Destroy();
+	}
+}
+
+void AProjectile::OnOverlapEnd(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+	UE_LOG(LogTemp, Warning, TEXT("OnOverlapEnd()"));
+}
---
+++ Source/alphaBuild/Projectile.h
@@ -0,0 +1,60 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Projectile.generated.h"
+
+UCLASS()
+class ALPHABUILD_API AProjectile : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AProjectile();
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Projectile")
+	class USphereComponent* HitBox;
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Projectile")
+	class UStaticMeshComponent* Mesh;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Projectile")
+	class UParticleSystemComponent* IdleParticlesComponent;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Projectile")
+	class UParticleSystem* OverlapParticles;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Projectile")
+	class USoundCue* OverlapSound;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, category = "Projectile")
+	float Damage;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, category = "Projectile")
+	float KnockBack;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, category = "Projectile")
+	float MovementSpeed;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, category = "Projectile")
+	float MaxLifeSpan;
+	UPROPERTY()
+	float CurrentLifeSpan;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, category = "Projectile")
+	bool bDestroyOnHit;
+	UPROPERTY()
+	FVector ForwardVector;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	UFUNCTION()
+	virtual void OnOverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	UFUNCTION()
+	virtual void OnOverlapEnd(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+
+};
