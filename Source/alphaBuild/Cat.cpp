eb3ecf73fe5b7917856babf0e56b1fbce77bdf17
BloodyFM <58073302+BloodyFM@users.noreply.github.com>
2020-05-01 13:20:43 +0200

    added a simple class for the cat
    will continue working on it

---
+++ Source/alphaBuild/Cat.cpp
@@ -0,0 +1,42 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Cat.h"
+#include "Components/SkeletalMeshComponent.h"
+#include "Main.h"
+
+
+ACat::ACat()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	SkeletalMesh = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("SkeletalMesh"));
+	SkeletalMesh->SetupAttachment(GetRootComponent());
+
+
+}
+
+void ACat::BeginPlay()
+{
+	Super::BeginPlay();
+
+}
+
+void ACat::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void ACat::OnOverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	Super::OnOverlapBegin(OverlappedComponent, OtherActor, OtherComp, OtherBodyIndex, bFromSweep, SweepResult);
+
+}
+
+void ACat::OnOverlapEnd(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+	Super::OnOverlapEnd(OverlappedComponent, OtherActor, OtherComp, OtherBodyIndex);
+
+}
---
+++ Source/alphaBuild/Cat.h
@@ -0,0 +1,39 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Item.h"
+#include "Cat.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ALPHABUILD_API ACat : public AItem
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	ACat();
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "SkeletalMesh")
+	class USkeletalMeshComponent* SkeletalMesh;
+	
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	UFUNCTION()
+	virtual void OnOverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+		UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+	UFUNCTION()
+	virtual void OnOverlapEnd(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+		UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+
+};
