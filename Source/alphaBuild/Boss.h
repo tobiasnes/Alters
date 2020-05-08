e83d6ee18f146e7d8085ba95107e9732aa6e72ef
BloodyFM <58073302+BloodyFM@users.noreply.github.com>
2020-05-08 14:42:27 +0200

    Setup boss with simple movement animations

---
+++ Source/alphaBuild/Boss.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Enemy.h"
+#include "Boss.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ALPHABUILD_API ABoss : public AEnemy
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/alphaBuild/BossAnimInstance.cpp
@@ -0,0 +1,39 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BossAnimInstance.h"
+#include "Boss.h"
+
+
+void UBossAnimInstance::NativeInitializeAnimation()
+{
+	if (Pawn == nullptr)
+	{
+		Pawn = TryGetPawnOwner();
+		if (Pawn)
+		{
+			Boss = Cast<ABoss>(Pawn);
+		}
+	}
+}
+
+void UBossAnimInstance::UpdateAnimationProperties()
+{
+	if (Pawn == nullptr)
+	{
+
+		Pawn = TryGetPawnOwner();
+		if (Pawn)
+		{
+			Boss = Cast<ABoss>(Pawn);
+		}
+
+	}
+
+	if (Pawn)
+	{
+		FVector Speed = Pawn->GetVelocity();
+		FVector LateralSpeed = FVector(Speed.X, Speed.Y, 0.f);
+		MovementSpeed = LateralSpeed.Size();
+	}
+}
