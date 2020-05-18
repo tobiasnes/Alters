0e97fba65898e2045c81b5a6f544d44b78b27200
BloodyFM <58073302+BloodyFM@users.noreply.github.com>
2020-05-18 18:27:10 +0200

    starded on boss

--- Source/alphaBuild/Boss.cpp
+++ Source/alphaBuild/Boss.cpp
@@ -2,4 +2,92 @@
 
 
 #include "Boss.h"
+#include "Main.h"
+#include "Components/SphereComponent.h"
+#include "Components/BoxComponent.h"
+#include "Kismet/KismetSystemLibrary.h"
+#include "Kismet/KismetMathLibrary.h"
+#include "TimerManager.h"
 
+
+ABoss::ABoss()
+{
+	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	AggroSphere = CreateAbstractDefaultSubobject<USphereComponent>(TEXT("AggroSphere"));
+	AggroSphere->SetupAttachment(GetRootComponent());
+	AggroSphere->InitSphereRadius(1000.f);
+
+	CombatTarget = nullptr;
+
+	bOverlappingAggrosphere = false;
+
+}
+
+void ABoss::BeginPlay()
+{
+	Super::BeginPlay();
+
+	SetBossMovementStatus(EBossMovementStatus::EMS_Idle);
+
+	//AggroSphere->OnComponentBeginOverlap.AddDynamic(this, &ABoss::AggroSphereOnOverlapBegin);
+	//AggroSphere->OnComponentEndOverlap.AddDynamic(this, &ABoss::AggroSphereOnOverlapEnd);
+
+}
+
+void ABoss::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void ABoss::AggroSphereOnOverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	if (OtherActor)
+	{
+		AMain* Main = Cast<AMain>(OtherActor);
+		if (Main)
+		{
+			if (!CombatTarget)
+			{
+				CombatTarget = Main;
+				TeleportBehindCombatTarget();
+				SetBossMovementStatus(EBossMovementStatus::EMS_Teleport);
+			}
+			else
+			{
+				SetBossMovementStatus(EBossMovementStatus::EMS_MidRange);
+			}
+			bOverlappingAggrosphere = true;
+		}
+	}
+}
+
+void ABoss::AggroSphereOnOverlapEnd(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+	if (OtherActor)
+	{
+		AMain* Main = Cast<AMain>(OtherActor);
+		if (Main)
+		{
+			bOverlappingAggrosphere = false;
+			TeleportBehindCombatTarget();
+			SetBossMovementStatus(EBossMovementStatus::EMS_Teleport);
+
+		}
+	}
+}
+
+void ABoss::TeleportBehindCombatTarget()
+{
+	FVector CurentPosition = GetActorLocation();
+	FVector CombatTargetLocation = CombatTarget->GetActorLocation();
+
+	FRotator ToCombatTargetRotation = UKismetMathLibrary::FindLookAtRotation(CurentPosition, CombatTargetLocation);
+	FRotator YawToCombatTargetRotation = FRotator(0.f, ToCombatTargetRotation.Yaw, 0.f);
+	// get forward vector
+	FVector Direction = FRotationMatrix(YawToCombatTargetRotation).GetUnitAxis(EAxis::X);
+
+	SetActorLocation(CombatTargetLocation + (Direction * 100.f));
+}
--- Source/alphaBuild/Boss.h
+++ Source/alphaBuild/Boss.h
@@ -6,6 +6,18 @@
 #include "Enemy.h"
 #include "Boss.generated.h"
 
+UENUM(BlueprintType)
+enum class EBossMovementStatus :uint8
+{
+	EMS_Idle			UMETA(DisplayName = "Idle"),
+	EMS_Teleport		UMETA(DisplayName = "Teleport"),
+	EMS_MidRange		UMETA(DisplayName = "MidRange"),
+	EMS_Melee			UMETA(DisplayName = "Melee"),
+
+	EMS_MAX				UMETA(DisplayName = "DefaultMAX")
+};
+
+
 /**
  * 
  */
@@ -13,5 +25,39 @@ UCLASS()
 class ALPHABUILD_API ABoss : public AEnemy
 {
 	GENERATED_BODY()
-	
+
+public:
+
+	ABoss();
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Movement")
+	EBossMovementStatus BossMovementStatus;
+
+	FORCEINLINE void SetBossMovementStatus(EBossMovementStatus Status) { BossMovementStatus = Status; }
+
+	// Sphere that will aggro the ai if the player overlaps
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "AI")
+	class USphereComponent* AggroSphere;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "AI")
+	class AMain* CombatTarget;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "AI")
+	bool bOverlappingAggrosphere;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	UFUNCTION()
+	virtual void AggroSphereOnOverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+	UFUNCTION()
+	virtual void AggroSphereOnOverlapEnd(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+
+	void TeleportBehindCombatTarget();
+
 };
